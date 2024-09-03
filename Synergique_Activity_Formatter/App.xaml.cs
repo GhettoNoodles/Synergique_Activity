6dda9ce390a005c25da05f7545040cc167f68f61
Jacques Coetzee <jcoetzee2201@gmail.com>
2024-09-03 19:00:16 +0200

    Initial Commit

---
+++ Synergique_Activity_Formatter/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Synergique_Activity_Formatter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Synergique_Activity_Formatter/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Navigation;
+
+namespace Synergique_Activity_Formatter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow:NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
