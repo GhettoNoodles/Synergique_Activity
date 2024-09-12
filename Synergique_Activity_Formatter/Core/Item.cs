687f347590ad401a9363002a71debfe3ca784d50
Jacques Coetzee <jcoetzee2201@gmail.com>
2024-09-12 09:27:00 +0200

    Cleanup

--- Synergique_Activity_Formatter/Core/Item.cs
+++ Synergique_Activity_Formatter/Core/Item.cs
@@ -5,7 +5,10 @@ namespace Synergique_Activity_Formatter.Core
         public string Name;
         public float[] Sales = new float[12];
         public float AverageSales;
+        public string LastInvoiced;
+        public string LastPurchased;
         public int CurrentStock;
         public int FormulaDrivenOrder;
+        
     }
 }
--- Synergique_Activity_Formatter/MainWindow.xaml.cs
+++ Synergique_Activity_Formatter/MainWindow.xaml.cs
@@ -8,7 +8,9 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Windows.Media.Animation;
+using Bytescout.Spreadsheet.Constants;
 using Synergique_Activity_Formatter.Core;
+using System.Text.Json;
 
 namespace Synergique_Activity_Formatter
 {
@@ -16,12 +18,14 @@ namespace Synergique_Activity_Formatter
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
-    {  public string newSheetPath;
+    {
+        public string newSheetPath;
+        private List<Item> _items = new List<Item>();
+
         public MainWindow()
         {
-            
             InitializeComponent();
-          //  ReadData("C:\\Users\\jacqu\\RiderProjects\\Synergique_Activity\\Synergique_Activity_Formatter\\Copy of Activity Sumary 04-04-2023 Excel - Copy.xlsx");
+            //  ReadData("C:\\Users\\jacqu\\RiderProjects\\Synergique_Activity\\Synergique_Activity_Formatter\\Copy of Activity Sumary 04-04-2023 Excel - Copy.xlsx");
         }
 
 
@@ -31,7 +35,7 @@ namespace Synergique_Activity_Formatter
             var dialog = new Microsoft.Win32.OpenFileDialog();
             dialog.FileName = "Document"; // Default file name
             dialog.DefaultExt = ".xlsx"; // Default file extension
-            dialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";// Filter files by extension
+            dialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm"; // Filter files by extension
 
 // Show open file dialog box
             bool? result = dialog.ShowDialog();
@@ -40,37 +44,50 @@ namespace Synergique_Activity_Formatter
             if (result == true)
             {
                 // Open document
-                newSheetPath= dialog.FileName;
+                newSheetPath = dialog.FileName;
                 ReadData();
             }
         }
-         public void ReadData()
+
+        public void ReadData()
         {
-            string prevCellTxt = "";
             string currentCellTxt = "";
             int salesCol = 3;
-            int onHandCol = 4;
-            int currentLine = 2;// row of first item(SkipHeading)
-            bool finishFlag = false;
+            int currentLine = 2; // row of first item(SkipHeading)
             Spreadsheet document = new Spreadsheet();
             document.LoadFromFile(newSheetPath);
             Worksheet incomingData = document.Workbook.Worksheets.ByName("Data1");
-            while (!finishFlag)
+            int TimeoutCounter = 0;
+            while (TimeoutCounter < 15000)
             {
+                TimeoutCounter++;
                 var currentCell = incomingData.Cell(currentLine, 0);
                 currentCellTxt = currentCell.ValueAsString;
-                
+
                 //item Name
                 Item newItem = new Item();
                 newItem.Name = currentCellTxt;
                 Console.WriteLine(newItem.Name);
-                while (currentCellTxt!="March")
+
+                //invoiced
+                currentLine++;
+                currentCell = incomingData.Cell(currentLine, 0);
+                newItem.LastInvoiced = currentCell.ValueAsString;
+                Console.WriteLine(newItem.LastInvoiced);
+                //Purchased
+                currentLine++;
+                currentCell = incomingData.Cell(currentLine, 0);
+                newItem.LastPurchased = currentCell.ValueAsString;
+                Console.WriteLine(newItem.LastPurchased);
+
+                //Sales
+                while (currentCellTxt != "March")
                 {
                     currentLine++;
                     currentCell = incomingData.Cell(currentLine, 0);
                     currentCellTxt = currentCell.ValueAsString;
                 }
-                //Item salesArray
+
                 float[] sales = new float[12];
                 for (int i = 0; i < 12; i++)
                 {
@@ -78,26 +95,91 @@ namespace Synergique_Activity_Formatter
                     sales[i] = currentCell.ValueAsInteger;
                     currentLine++;
                 }
+
                 newItem.Sales = sales;
                 foreach (var val in newItem.Sales)
                 {
                     Console.WriteLine(val);
                 }
-                newItem.AverageSales = sales.Sum() / 12;
-                Console.WriteLine("Average = "+ newItem.AverageSales);
-                //currentStock
-                //currentLine++;//unposted
-                //currentCell = incomingData.Cell(currentLine, onHandCol);
-               // newItem.CurrentStock = currentCell.ValueAsInteger;
-                
-                currentLine+=2;//should be next inventory item
+
+                //AverageSales
+                newItem.AverageSales = (float)Math.Ceiling(sales.Sum() / 12f);
+                Console.WriteLine("Average = " + newItem.AverageSales);
+                _items.Add(newItem);
+                currentLine += 2; //should be next inventory item
                 currentCell = incomingData.Cell(currentLine, 0);
                 if (currentCell.ValueAsString == "")
                 {
-                    finishFlag = true;
                     break;
                 }
             }
+        }
+
+        private void CompareAndSerializeJsons(List<Item>items)
+        {
+            
+        }
+        
+
+        private void Save_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Create new Spreadsheet
+            Spreadsheet outputDoc = new Spreadsheet();
+
+            // add new worksheet
+            Worksheet orderSheet = outputDoc.Workbook.Worksheets.Add("FormulaDemo");
+            
+            //Columns for initial calculations
+            orderSheet.Cell("A1").Value =
+                "Reve Holdings SA (Pty) Ltd - Equipment  |  Prepared by:  Reve Holdings SA (Pty) Ltd";
+            orderSheet.Rows[0].BottomBorderStyle = LineStyle.Thick;
+            orderSheet.Columns[0].Width = 520;
+            orderSheet.Cell("B1").Value = "12 Month Average";
+            orderSheet.Columns[1].Width = 70;
+            orderSheet.Columns[1].AlignmentHorizontal = AlignmentHorizontal.Left;
+            orderSheet.Cell("C1").Value = "Minimum Level";
+            orderSheet.Columns[2].Width = 70;
+            orderSheet.Columns[2].AlignmentHorizontal = AlignmentHorizontal.Left;
+            orderSheet.Cell("D1").Value = "Maximum Level";
+            orderSheet.Columns[3].Width = 70;
+            orderSheet.Columns[3].AlignmentHorizontal = AlignmentHorizontal.Left;
+            orderSheet.Rows[0].Height = 40;
+            orderSheet.Rows[0].Wrap = true;
+            orderSheet.Rows[0].AlignmentVertical = AlignmentVertical.Centered;
+
+            //Adding values for each item
+            var nameLine = 1;
+            foreach (var item in _items)
+            {
+                orderSheet.Cell(nameLine, 0).Value = item.Name;
+
+                orderSheet.Cell(nameLine + 1, 0).Value = item.LastInvoiced;
+
+                orderSheet.Cell(nameLine + 2, 0).Value = item.LastPurchased;
+                orderSheet.Cell(nameLine + 2, 1).Value = item.AverageSales;
+                orderSheet.Cell(nameLine + 2, 2).Value = item.AverageSales*3;
+                orderSheet.Cell(nameLine + 2, 3).Value = item.AverageSales*6;
+                orderSheet.Rows[nameLine + 2].BottomBorderStyle = LineStyle.Thin;
+                
+                
+                nameLine += 3;//next item
+                
+            }
+
+            // delete output file if exists already
+            if (File.Exists("Output.xls"))
+            {
+                File.Delete("Output.xls");
+            }
+
+            // Save document
+            outputDoc.SaveAs("Output.xls");
+
+            // Close Spreadsheet
+            outputDoc.Close();
+
+            // open generated XLS document in default program
+            Process.Start("Output.xls");
         }
     }
 }
